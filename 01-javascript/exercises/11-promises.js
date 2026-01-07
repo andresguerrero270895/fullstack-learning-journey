8805481d94b36994a3169fc25eeb25d9abcfe8ab
andresguerrero270895 <guerrero9510@hotmail.com>
2026-01-07 00:23:55 -0500

    feat(exercises): add exercise to convert callbacks to promises

--- 01-javascript/exercises/11-promises.js
+++ 01-javascript/exercises/11-promises.js
@@ -41,3 +41,36 @@ myFirstPromise.then((result) => {
 }).catch((error) => {
   console.log("Error", error)
 });
+
+//=== EXAMPLE 2 : Converting callbacks to Promises
+
+//Function with callback (old way)
+
+function getUserCallback(id, callback) {
+  setTimeout(() => { 
+    callback({id: id, name: "Andres"});
+  }, 2000);
+}
+
+//Same function with promises
+function getUserPromise(id) {
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      if(id <= 0) {
+        reject("Invalid ID")
+        return;
+      }
+      resolve({id: id, name: "Andres"});
+    }, 2000);
+  });
+}
+
+//Using the promise Version:
+getUserPromise(1)
+.then((user) => {
+  console.log("User retrieved", user);
+})
+.catch((error) => {
+  console.log("Error", error);
+});
+
