4a79ef2457bf04ab1934d625f8d23e8330665162
andresguerrero270895 <guerrero9510@hotmail.com>
2026-01-07 00:02:23 -0500

    introduce the new concept of promises

---
+++ 01-javascript/exercises/11-promises.js
@@ -0,0 +1,16 @@
+// === PROMISES THE ELEGANT SOLUTION ===
+
+/*
+ A promise is an object that represents the FUTURE
+ result of an asynchronous operation.
+
+ States of a promise : 
+ -PENDING: Waiting (has'bt finished yet)
+ -FULLFILLED: Completed (finished successfully)
+ -REJECTED: Rejected (there was an error)
+
+ It's like when you order food for delivery: 
+ -They give you a tracking number(promise)
+ -Status: "Being prepared" (pending)
+ -Status : "Delivered" (fulfilled) or "canceled" (Rejected)
+ */
