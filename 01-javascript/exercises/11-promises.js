87563251a7453d88a17f3349ca0c0471598a3cb0
andresguerrero270895 <guerrero9510@hotmail.com>
2026-01-07 00:12:17 -0500

    feat(exercises): add promises content

--- 01-javascript/exercises/11-promises.js
+++ 01-javascript/exercises/11-promises.js
@@ -14,3 +14,30 @@
  -Status: "Being prepared" (pending)
  -Status : "Delivered" (fulfilled) or "canceled" (Rejected)
  */
+
+console.log("\n === PROMISES ===");
+
+// EXAMPLE : Creating a Promise 
+
+const myFirstPromise = new Promise ((resolve, reject) => {
+  // resolve = call when everything went well 
+  // reject = call when there was an error
+
+  const sucess = true; // change to false to see the error
+
+  setTimeout(() => {
+    if(sucess) {
+      resolve("Operation successful!"); //Fullfilled
+    } else {
+      reject("Something went wrong"); //Rejected
+    }
+  }, 1000);
+});
+
+//Using the promises with .then() and catch()
+
+myFirstPromise.then((result) => {
+  console.log("Success:", result);
+}).catch((error) => {
+  console.log("Error", error)
+});
