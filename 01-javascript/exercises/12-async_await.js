8f3835bd0546e11688f69fa3c4a8252617eccbba
andresguerrero270895 <guerrero9510@hotmail.com>
2026-01-09 00:01:13 -0500

    feat(exercises): Async/await function

--- 01-javascript/exercises/12-async_await.js
+++ 01-javascript/exercises/12-async_await.js
@@ -54,3 +54,39 @@ async function process() {
 
 //Execute
 process().then(console.log);
+
+//EXAMPLE 3 : Async/Await vs Promises 
+function getData(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ id, name : `User ${id}`});
+    }, 500);
+  });
+}
+
+//with promises (more verbose)
+function getUserWithPromises (){
+  return getData(1). then((user => {
+    console.log(`Promise - User`, user);
+    return getData(2);
+  }))
+  .then((user2) => {
+    console.log("Promise - User 2", user2);
+    return "Completed"
+  });
+}
+
+//With async/await (cleanner)
+async function getUserWithAsync() {
+  const user = await getData(1);
+  console.log("Async - User", user);
+
+  const user2 = await getData(2);
+  console.log("Async - User 2:", user2);
+
+  return "Completed";
+  
+}
+
+getUserWithAsync().then(console.log);
+
